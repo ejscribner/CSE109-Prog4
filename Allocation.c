876351c8f785fb7aa7fe8d709f533dc37b2f032e
Elliot Scribner <ejs320@xena.cse.lehigh.edu>
2018-10-22 14:42:20 -0400

    master pushes

--- Allocation.c
+++ Allocation.c
@@ -7,27 +7,81 @@ Program #4
 */
 
 #include "Allocation.h"
+#include <stdio.h>
 
-void makeAllocation(struct Allocation_t* this, size_t start, size_t size) {
-
+void makeAllocation(struct Allocation_t *this, size_t start, size_t size)
+{
+	this->start = start;
+	this->size = size;
 }
 
-void freeAllocation(struct Allocation_t* this) {
-
+void freeAllocation(struct Allocation_t *this)
+{
+	this->start = 0;
+	this->size = 0;
 }
 
-size_t getStart(struct Allocation_t* this) {
-	return 1;
+size_t getStart(struct Allocation_t *this)
+{
+	return this->start;
 }
 
-size_t getEnd(struct Allocation_t* this) {
-	return 1;
+size_t getEnd(struct Allocation_t *this)
+{
+	size_t end = this->start + this->size;
+	return end;
 }
 
-size_t getSize(struct Allocation_t* this) {
-	return 1;
+size_t getSize(struct Allocation_t *this)
+{
+	return this->size;
 }
 
-int doesOverlap(struct Allocation_t* this) {
-	return 0;
+int doesOverlap(struct Allocation_t *this, size_t start, size_t size)
+{
+	int doesOverlap = 0;
+	int min = 0;
+	int max = 0;
+	int thisStart = getStart(this);
+	int thisSize = getSize(this);
+	int endpt1 = thisStart + thisSize;
+	int endpt2 = start + size;
+
+	if (thisStart < start)
+	{
+		min = thisStart;
+	}
+	else if (start < thisStart)
+	{
+		min = start;
+	}
+	else if (start == thisStart)
+	{
+		doesOverlap = 1;
+		return doesOverlap;
+	}
+
+	if (endpt1 < endpt2)
+	{
+		max = endpt2;
+	}
+	else if (endpt2 < endpt1)
+	{
+		max = endpt1;
+	}
+	else if (endpt1 == endpt2)
+	{
+		doesOverlap = 1;
+		return doesOverlap;
+	}
+
+	int fullSize = size + thisSize;
+	int range = max - min;
+	if (range < fullSize)
+	{
+		doesOverlap = 1;
+		return doesOverlap;
+	}
+
+	return doesOverlap;
 }
---
+++ checkpoint4.c
@@ -0,0 +1,96 @@
+/*
+CSE 109: Fall 2018
+Elliot Scribner
+ejs320
+This program is a non-optimzed memory allocator to fulfil memory requests made by users
+Program #4
+*/
+
+#ifndef ALLOCATION_H
+#define ALLOCATION_H
+#include <stdlib.h>
+
+struct Allocation_t {
+	size_t start;
+	size_t size;
+};
+
+void makeAllocation(struct Allocation_t* this, size_t start, size_t size);
+
+void freeAllocation(struct Allocation_t* this);
+
+size_t getStart(struct Allocation_t* this);  //Check if this vs it matters
+
+size_t getEnd(struct Allocation_t* this);
+
+size_t getSize(struct Allocation_t* this);
+
+int doesOverlap(struct Allocation_t* this, size_t start, size_t size);
+
+#endif
+/*
+CSE 109: Fall 2018
+Elliot Scribner
+ejs320
+This program is a non-optimzed memory allocator to fulfil memory requests made by users
+Program #4
+*/
+
+#include "Allocation.h"
+#include <stdio.h>
+
+void makeAllocation(struct Allocation_t* this, size_t start, size_t size) {
+	this->start = start;
+	this->size = size;
+}
+
+void freeAllocation(struct Allocation_t* this) {
+	this->start = 0;
+	this->size = 0;
+}
+
+size_t getStart(struct Allocation_t* this) {
+	return this->start;
+}
+
+size_t getEnd(struct Allocation_t* this) {
+	size_t end = this->start + this->size;
+	return end;
+}
+
+size_t getSize(struct Allocation_t* this) {
+	return this->size;
+}
+
+int doesOverlap(struct Allocation_t* this, size_t start, size_t size) {
+	int doesOverlap = 0;
+	int min = 0;
+	int max = 0;
+	int thisStart = getStart(this);
+	int thisSize = getSize(this);
+	int endpt1 = thisStart+thisSize;
+	int endpt2 = start + size;
+	if (thisStart < start) {
+		min = thisStart;
+	} else if (start < thisStart) {
+		min = start;
+	} else if (start == thisStart) {
+		doesOverlap = 1;
+		return doesOverlap;
+	}
+	if (endpt1 < endpt2) {
+		max = endpt2;
+	} else if (endpt2 < endpt1) {
+		max = endpt1;
+	} else if (endpt1 == endpt2) {
+		doesOverlap = 1;
+		return doesOverlap;
+	}
+	int fullSize = size + thisSize;
+	int range = max - min;
+	if (range < fullSize) {
+		doesOverlap = 1;
+		return doesOverlap;
+	}
+	return doesOverlap;
+}
